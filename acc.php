ae30d4bddcda41a79419e01167f5bef34ae102ee
youkoal <ticapron@hotmail.fr>
2015-09-19 13:13:16 +0400

    cleaning
    corrections de petites erreurs et nettoyages de certain aspect du code

--- acc.php
+++ acc.php
@@ -41,73 +41,69 @@ elseif (isset($_POST['annuler'])) // Si on a voulu annulé une action.
 {
   header('Location: acc.php');
 }
+else{
+  //utilisateur connecté
+  if (isset($perso)){
+    if(isset($_GET['get_param'])){//demande d'infos pour remplissage des champs
+      if($_GET['get_param']=='users'){
+        makeResponseUsers($perso, $manager);
+      }
+      elseif($_GET['get_param']=='editPerso'){
+        makeResponseEdit($perso);
+      }
+      elseif($_GET['get_param']=='minTaches'){
+        makeResponseMinTaches($perso,$tmanager);
+      }
+      elseif($_GET['get_param']=='bigTaches'){
+        makeResponseBigTaches($perso,$tmanager,$_GET['idTask']);
+      }
+      elseif($_GET['get_param']=='tache'){
+        makeResponseEditTaches($perso,$tmanager);
+      }
+    }
+    //condition à optimisée
+    elseif(isset($_GET['ed']) && !isset($_POST['editerTacheOK']) && !isset($_POST['annuler']) )
+    {
+      $RService->renderEditTache();
+    }
+    else
+    {
+      if (isset($_POST['editer']))//requete formulaire d'édition d'un client.
+      {
+        editRender($RService);
+      }
+      elseif(isset($_POST['editerOK']))//requete mise à jour du client.
+      {//mise à jour du perso
+        editSending($perso,$manager);
+      }
+      elseif (isset($_POST['creeTache'])) // requete formulaire création d'une nouvelle tache.
+      {
+        tacheForm($RService);
+      }
+      elseif (isset($_POST['creeTacheOK'])) // requete ajout d'une tache.
+      {
+        creeSendTask($tmanager,$perso->id());
+      }
+      elseif(isset($_POST['editerTache'])) // requete formulaire d'édition d'une tâche
+      {
+        setEditTask();
+      }
+      elseif(isset($_POST['editerTacheOK'])) // requete mise à jour d'une tache
+      {//mise à jour du perso
+        editSendTask($tmanager,$perso->id());
+      }
+      elseif(isset($_POST['delTask']))  // requete suppression d'une tâche
+      {//mise à jour du perso
+        delTask($tmanager,$perso->id());
+      }
+      else// Si aucune requetes, on affiche juste la page d'accueil apres login.
+      {
+        userRender($RService,$manager,$perso);
+      }
+    }
 
-
-
-//utilisateur connecté
-if (isset($perso)){
-  if(isset($_GET['get_param'])){//demande d'infos pour remplissage des champs
-    if($_GET['get_param']=='users'){
-      makeResponseUsers($perso, $manager);
-    }
-    elseif($_GET['get_param']=='editPerso'){
-      makeResponseEdit($perso);
-    }
-    elseif($_GET['get_param']=='minTaches'){
-      makeResponseMinTaches($perso,$tmanager);
-    }
-    elseif($_GET['get_param']=='bigTaches'){
-      makeResponseBigTaches($perso,$tmanager,$_GET['idTask']);
-    }
-    elseif($_GET['get_param']=='tache'){
-      makeResponseEditTaches($perso,$tmanager);
-    }
   }
-  //condition à optimisée
-  elseif(isset($_GET['ed']) && !isset($_POST['editerTacheOK']) && !isset($_POST['annuler']) )
-  {
-    $RService->renderEditTache();
-  }
-  else
-  {
-    if (isset($_POST['editer']))
-    {
-      editRender($RService);
-    }
-    elseif(isset($_POST['editerOK']))
-    {//mise à jour du perso
-      editSending($perso,$manager);
-    }
-    elseif (isset($_POST['creeTache'])) // Si on utilise un personnage (nouveau ou pas).
-    {
-      tacheForm($RService);
-    }
-    elseif (isset($_POST['creeTacheOK'])) // Si on utilise un personnage (nouveau ou pas).
-    {
-      creeSendTask($tmanager,$perso->id());
-    }
-    elseif(isset($_POST['editerTache']))
-    {
-      setEditTask();
-    }
-    elseif(isset($_POST['editerTacheOK']))
-    {//mise à jour du perso
-      editSendTask($tmanager,$perso->id());
-    }
-    elseif(isset($_POST['delTask']))
-    {//mise à jour du perso
-      delTask($tmanager,$perso->id());
-    }
-    else// Si on utilise un personnage (nouveau ou pas).
-    {
-      userRender($RService,$manager,$perso);
-    }
-  }
-
-
 }
-
-
 
 //formulaire de connection si l'utilisateur n'est pas connecter
 if(!(isset($_SESSION['perso'])))
@@ -115,6 +111,5 @@ if(!(isset($_SESSION['perso'])))
   //formulaire de connection
   $RService->renderForn(); 
 }
-
 
 ?>
